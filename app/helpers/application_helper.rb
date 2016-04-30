9fc4ff5e34ca801a94fd136d9f335fb880c11f24
Tahrim <utahrim@gmail.com>
2016-04-30 13:59:28 -0400

    fix loop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,7 +56,6 @@ module ApplicationHelper
     response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
         http.request(request)
     end
-
     json = JSON(response.body)["Results"]
 
     i = 0
@@ -64,13 +63,10 @@ module ApplicationHelper
     while i < count.to_i do
         score = {"kills" => json[i]["Players"].first["TotalKills"], "deaths" => json[i]["Players"].first["TotalDeaths"], "assists" => json[i]["Players"].first["TotalAssists"]}
         results << score
+        i += 1
     end
 
     return results
-
-    binding.pry
-  
-
 
 
   end
